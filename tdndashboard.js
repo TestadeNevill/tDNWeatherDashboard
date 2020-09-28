1a97c99c298af93eebabc66fa9db60ead3f9a538
TestadeNevill <64296826+TestadeNevill@users.noreply.github.com>
2020-09-28 00:43:01 -0400

    Added API Key and function that calls the weather data based on search variable.

--- tdndashboard.js
+++ tdndashboard.js
@@ -1,5 +1,7 @@
 //Wait for our html content to load before we run our js file.
 $(document).ready(function () {
+    //Api key for ajax url made global because it will be used in several functions.
+    const APIKEY = "01156022c7b693ce99c5e41d6e384ece";
     //create variable from user inputted city name for our api call.
     $("#search-button").on("click", function () {
         var searchValue = $("#search-value").val();
@@ -8,4 +10,16 @@ $(document).ready(function () {
         //Search Value is the City name
         searchWeather(searchValue);
     });
+    function searchWeather(searchValue) {
+        $.ajax({
+            type: "GET",
+            url: "http://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=" +
+                APIKEY +
+                "&units=imperial",
+            dataType: "json",
+            success: function (data) {
+                console.log(data)
+            },
+        });
+    }
 })
