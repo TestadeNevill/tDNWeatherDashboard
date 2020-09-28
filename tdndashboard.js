cf57704e3513d5b886a79d0b50946387e9e9e516
TestadeNevill <64296826+TestadeNevill@users.noreply.github.com>
2020-09-28 00:38:03 -0400

    Added search variable

--- tdndashboard.js
+++ tdndashboard.js
@@ -1,4 +1,11 @@
 //Wait for our html content to load before we run our js file.
 $(document).ready(function () {
-
+    //create variable from user inputted city name for our api call.
+    $("#search-button").on("click", function () {
+        var searchValue = $("#search-value").val();
+        // clear input box
+        $("#search-value").val("");
+        //Search Value is the City name
+        searchWeather(searchValue);
+    });
 })
